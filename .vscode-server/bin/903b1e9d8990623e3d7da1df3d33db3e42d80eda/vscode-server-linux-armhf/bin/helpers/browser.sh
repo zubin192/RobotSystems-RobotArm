f44d284a6f0a7ed191b3e349c61bf0c200c96145
zubin192 <zubin192@gmail.com>
2024-02-24 15:23:42 -0800

    Initial commit

---
+++ .vscode-server/bin/903b1e9d8990623e3d7da1df3d33db3e42d80eda/vscode-server-linux-armhf/bin/helpers/browser.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+#
+ROOT="$(dirname "$(dirname "$(dirname "$(readlink -f "$0")")")")"
+
+APP_NAME="code"
+VERSION="1.86.2"
+COMMIT="903b1e9d8990623e3d7da1df3d33db3e42d80eda"
+EXEC_NAME="code"
+CLI_SCRIPT="$ROOT/out/server-cli.js"
+"$ROOT/node" "$CLI_SCRIPT" "$APP_NAME" "$VERSION" "$COMMIT" "$EXEC_NAME" "--openExternal" "$@"
